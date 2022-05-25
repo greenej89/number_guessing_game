12a548880806f770e9e31a416d6453a2528539c0
Jessica Greene <jessica.greene89@gmail.com>
2022-05-25 16:15:11 -0400

    changed port to 5000

--- server.py
+++ server.py
@@ -42,5 +42,5 @@ def reset_game():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
-    # app.run(debug=True) For non-Mac users
+    # app.run(debug=True, port=5001)
+    app.run(debug=True) For non-Mac users
